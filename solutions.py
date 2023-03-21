5dcba3d7ee3cc342b8e172bd5412bf7e9c124cac
Carlina Kim <carlina.kim.ubc@gmail.com>
2023-03-21 13:58:09 -0700

    adding solutions

--- solutions.py
+++ solutions.py
@@ -1,3 +1,7 @@
+### SOLUTION
+# To run, `python solutions.py` in your terminal
+
+
 #import packages 
 
 import pandas as pd
@@ -12,6 +16,7 @@ import scipy as sp
 import plotly.express as px
 
 #reading in data 
+print('-----------------reading data!-----------------------')
 data = pd.read_csv("data/sample-data.csv")
 df= data.copy()
 
@@ -19,8 +24,10 @@ df= data.copy()
 gdf = convertWKT(df, 'Pair_a')
 gdf = convertWKT(df, 'Pair_b')
 
+print('-----------------converting geodataframe to calculations intersection, difference, and overlap area------------------')
 # new column for intersection of the two plots, like an inner join of polygon shapes
 gdf['intersect_plots'] = gdf['Pair_a'].intersection(gdf['Pair_b'])
+gdf[['id', 'intersect_plots']].to_csv('outputs/intersect_plots.csv', index= False)
 
 # new column for difference of the two plots, like an outer join of polygon shapes
 # Ones with POLYGON EMPTY will be flagged for project managers to address issue 
@@ -32,14 +39,20 @@ gdf['overlap_area_plotb_percent'] = percentage_area(gdf, 'Pair_b', 'Pair_a')
 # average of percentages
 gdf['overlap_average_percent'] = (gdf['overlap_area_plota_percent']+ gdf['overlap_area_plotb_percent'])/2
 
+print('-----------------generating threshold metric files-----------------------')
 #setting threshold metrics for different overlaps 
-union_overlaps = gdf.query('overlap_average_percent <= 5').reset_index(drop= True)
-needs_review_overlaps = gdf.query('5 < overlap_average_percent & overlap_average_percent <= 95').reset_index(drop= True)
+union_overlaps = gdf.query('overlap_average_percent < 5').reset_index(drop= True)
+needs_review_overlaps = gdf.query('5 <= overlap_average_percent & overlap_average_percent <= 95').reset_index(drop= True)
 full_overlaps = gdf.query('overlap_average_percent > 95').reset_index(drop= True)
 
 # Union 
 union_overlaps['union'] = union_overlaps['Pair_a'].union(union_overlaps['Pair_b'])
+union_overlaps_df = union_overlaps[['id', 'intersect_plots', 'overlap_average_percent', 'union']]
+union_overlaps_df.to_csv('outputs/union_overlaps_df.csv', index= False)
 
 #saving full overlaps 
 full_overlaps_df = full_overlaps[['id', 'Pair_a', 'Pair_b', 'intersect_plots', 'overlap_average_percent']]
 full_overlaps_df.to_csv('outputs/full_overlaps_df.csv', index= False)
+
+print('-----------------saving outputs!-----------------------')
+print('-----------------done! file run complete-----------------------')
